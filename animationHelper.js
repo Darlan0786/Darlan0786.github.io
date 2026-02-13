087de0fb9c1360b922c219c7397f5e6a80433d4b
Darlan0786 <darlanana06@gmail.com>
2026-02-13 01:09:18 -0300

    Implement play and stop animation functions
    Added functions to play and stop animations using Three.js.

--- animationHelper.js
+++ animationHelper.js
@@ -1,1 +1,14 @@
+// animationHelper.js
+// Gerencia animações do Three.js e GLTFLoader
 
+export function playAnimation(mixer, actionName, loop = true) {
+    const action = mixer.clipAction(actionName);
+    if (loop) action.setLoop(THREE.LoopRepeat);
+    action.reset();
+    action.play();
+}
+
+export function stopAnimation(mixer, actionName) {
+    const action = mixer.clipAction(actionName);
+    action.stop();
+}
