dfb96ad79cd1b064d3e320c5ff69c52dfa542c7a
Darlan0786 <darlanana06@gmail.com>
2026-02-13 01:04:15 -0300

    Add game constants and parameters

--- constants.js
+++ constants.js
@@ -1,1 +1,23 @@
+// constants.js
+// Valores globais e parâmetros do jogo
 
+export const GAME = {
+    GRAVITY: -9.81,
+    PLAYER_SPEED: 5,
+    PLAYER_JUMP: 8,
+    CAMERA_FOLLOW_SPEED: 0.1,
+    MAX_ISLANDS: 5
+};
+
+export const HUD = {
+    HEALTH_BAR_WIDTH: 200,
+    HEALTH_BAR_HEIGHT: 20,
+    MANA_BAR_WIDTH: 200,
+    MANA_BAR_HEIGHT: 20
+};
+
+export const GACHA = {
+    RATE_4STAR: 0.7,
+    RATE_5STAR: 0.25,
+    RATE_6STAR: 0.05
+};
