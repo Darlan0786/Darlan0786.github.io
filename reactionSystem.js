969ad5bca639182be2d23a8c091ad7609075c927
Darlan0786 <darlanana06@gmail.com>
2026-02-22 01:53:49 -0300

    Add REACTIONS constant for elemental effects

--- reactionSystem.js
+++ reactionSystem.js
@@ -1,1 +1,12 @@
-
+export const REACTIONS = Object.freeze({
+  "fire+wind": {
+    name: "Inferno Burst",
+    multiplier: 1.8,
+    effect: "burn"
+  },
+  "ice+electric": {
+    name: "Shatter Shock",
+    multiplier: 1.6,
+    effect: "stun"
+  }
+});
